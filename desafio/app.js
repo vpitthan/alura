772c1a22c39bd60c5e9e5c551fd02e3542904325
vpitthan <viniciuspitthan74@gmail.com>
2025-01-06 16:00:47 -0300

    Update app.js

--- desafio/app.js
+++ desafio/app.js
@@ -3,10 +3,10 @@ let nome = ("Lua");
 let idade = 25;
 let numeroDeVendas = 50;
 let saldoDisponivel = 1000;
-let mensagemDeErro = ('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
 alert mensagemDeErro;
 let nome = prompt("Qual seu nome?");
 let idade = prompt('Qual sua idade?');
 if(idade >= 18){
-    alert("Pode tirar a habilitação!")
+    alert("Pode tirar a habilitação!");
 }
