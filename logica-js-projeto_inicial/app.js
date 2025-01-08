59fe9dcd798c9035460e81182711f2063bb32aca
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-01-08 07:15:45 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,9 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-let numeroSecreto2 = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 4;
+console.log = numeroSecreto;
+let numeroSecreto2 = prompt('Escolha um número entre 1 e 10'); 
 
 if (numeroSecreto == numeroSecreto2 ){
-    console.log('Isso ai! Você descobriu o número secreto (5)')
+    alert(`Isso ai! Você descobriu o número secreto` ${numeroSecreto});
 } else{
-    alert('Você errou :(')
+    alert('Você errou :(');
 }
