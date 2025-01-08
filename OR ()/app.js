72801f5dc34120ffa96b517b00b6e2f3bacf07e0
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-01-08 09:08:41 -0300

    comit

---
+++ OR ()/app.js
@@ -0,0 +1,7 @@
+let numeroMaximo = prompt("Digite um número para a contagem progregressiva:");;
+let contador = 0;
+
+while (contador <= numeroMaximo) {
+    console.log(contador);
+    contador++
+}
