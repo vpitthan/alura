8b19d4ad88ae1abf747fbbad572dba0f06943d04
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-01-08 07:44:24 -0300

    d003

--- d003/app.js
+++ d003/app.js
@@ -1,8 +1,6 @@
-let resposta = prompt('Qual o dia da semana?');
-if (resposta == 'Sabado'){
-    alert('Bom final de semana!')
-} else if (resposta == 'Domingo'){
-    alert('Bom final de semana!')
-} else{
-    alert('Boa semana!')
+let numeroInformado = prompt('Escolha um número')
+if(numeroInformado >=0){
+    alert('Número positivo!')
+}else{
+    alert('Número negativo!')
 }
