e9939d3e7c814febd48ce89361c51c40093a7d5c
vpitthan <viniciuspitthan74@gmail.com>
2025-01-16 17:11:50 -0300

    0

---
+++ LOGICAJS003/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,21 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+
+    let ate = parseInt(document.getElementById('ate').value);
+    
+    let sorteados = [];
+    let numero;
+
+    for(let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML=`<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label> `
+
+}
+
+function obterNumeroAleatorio(min,max){
+    return parseInt(Math.random() * (max - min) + min); 
+}
