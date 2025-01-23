5e6e16e847e9ad6619b235f5f0e12807fb4c0f20
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-01-23 06:44:02 -0300

    aula1

--- JS/aula001/variaveis.js
+++
@@ -1,9 +0,0 @@
-let chute = prompt('Dudu é gay?');
-let resposta = 'sim';
-while(chute != resposta){
-alert('Mentira');
-chute = prompt('Dudu é gay?');
-}
-if(chute == resposta){
-    alert('Verdade');
-}
