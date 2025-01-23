81c55565b6a0b5dfc6e0f6547f7de73516e21ba3
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-01-23 06:21:39 -0300

    t1

--- JS/aula002/java.js
+++ JS/aula002/java.js
@@ -1,11 +1,3 @@
-let resposta = prompt('Duda é linda?')
-let resultado = 'sim'
-
-while(resposta != resultado){
-    alert('Mentira')
-    resposta = prompt('Duda é linda?')
+function clicar(){
     
 }
-if(resposta == resultado){
-        alert('Verdade')
-    } 
