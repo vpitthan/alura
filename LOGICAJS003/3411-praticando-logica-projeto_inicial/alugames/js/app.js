98a1dbd2403c91b854dc8c75e226002dfd719bf3
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-01-29 15:05:29 -0300

    0

---
+++ LOGICAJS003/3411-praticando-logica-projeto_inicial/alugames/js/app.js
@@ -0,0 +1,11 @@
+function alterarStatus(id){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    if(){
+
+    }else{
+        
+    }
+}
