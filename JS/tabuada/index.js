08efd36c5810acbb5c81d7596be0aada442c74b0
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-01-30 17:36:16 -0300

    tabuada

---
+++ JS/tabuada/index.js
@@ -0,0 +1,7 @@
+/*for (i = 0;i <= 10;i++){
+    console.log(`8x ${i} =`+ (8*i) )
+}*/
+for (let i = 0; i <= 10; i++) {
+    console.log(`8 x ${i} = ${8 * i}`);
+  }
+  
---
+++ JS/um-a-N/index.js
@@ -0,0 +1,4 @@
+let n = parseInt(prompt('Até que valor quer a lista?'));
+for(contador = 1;contador <= n;contador++){
+    console.log(contador);
+}
