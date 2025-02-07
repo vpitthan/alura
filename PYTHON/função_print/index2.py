aa3eb1e11ddad88a054e681f9d914a458e070500
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-02-07 16:33:55 -0300

    python

---
+++ PYTHON/função_print/index2.py
@@ -0,0 +1,4 @@
+nome = input('Qual seu nome?')
+idade = input('Qual sua idade?')
+
+print(f'Meu nome é {nome} e tenho {idade} anos')
---
+++ PYTHON/função_print/index4.py
@@ -0,0 +1,3 @@
+pi = 3.14159
+pi = int(pi)
+print('O valor arredondado de pi é:', pi)
