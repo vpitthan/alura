40d7d5fd5c3e5294553aa6e53f0834ae7d47b5bf
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-02-10 15:59:09 -0300

    js

---
+++ NODE/001.js
@@ -0,0 +1,9 @@
+// Declarando variáveis
+let nome = "João";       // Tipo string
+let idade = 25;          // Tipo number
+let isEstudante = true;  // Tipo boolean
+
+// Exibindo o tipo de cada variável
+console.log( nome);        // "string"
+console.log( idade);       // "number"
+console.log( isEstudante); // "boolean"
---
+++ NODE/escopo.js
@@ -0,0 +1,7 @@
+let estudante;
+
+if (1>0){
+    estudante ='Caroline';
+    console.log(estudante);
+}
+console.log(estudante);
