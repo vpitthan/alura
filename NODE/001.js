4ac3e739c77dca0cd41cb85d16fd264f202a2fb1
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-02-10 17:54:29 -0300

    Update 001.js

--- NODE/001.js
+++ NODE/001.js
@@ -1,9 +1,45 @@
-// Declarando variáveis
-let nome = "João";       // Tipo string
-let idade = 25;          // Tipo number
-let isEstudante = true;  // Tipo boolean
+class Person {
+    name = ''
+    age = 0
+    gender = ''
 
-// Exibindo o tipo de cada variável
-console.log( nome);        // "string"
-console.log( idade);       // "number"
-console.log( isEstudante); // "boolean"
+    constructor(name, age, gender) {
+        this.name = name
+        this.age = age
+        this.gender = gender
+    }
+
+    getPerson() {
+        console.log({
+            name: this.name,
+            age: this.age,
+            gender: this.gender
+        })
+    }
+}
+
+const person = new Person("Vinicius", 20, 'Masculino')
+person.getPerson()
+
+const person2 = new Person("Victor", 24, "Masculino")
+person2.getPerson()
+
+// function sum(num1, num2) {
+//     return num1 + num2
+// }
+
+// console.log(sum(1,2))
+
+// const arrowFunctionSum = (num1, num2) => num1 + num2;
+
+// console.log(arrowFunctionSum(2, 3))
+
+// const operations = {
+//     SUM: (a, b) => a + b,
+//     MULTIPLY: (a, b) => a * b,
+//     DIVIDE: (a, b) => a / b
+// }
+
+// console.log(operations['SUM'](10, 10))
+
+// Estudar 
