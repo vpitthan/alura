aca19e95d5a908b7c6b757ec2cd3e409279ffcfd
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-02-11 16:30:20 -0300

    comit

---
+++ NODE/cap001/tipo-string.js
@@ -0,0 +1,14 @@
+const estudante = 'vinicius';
+const docente = 'dudu';
+const cumprimento = "Nosso lema é ' estudar bastante! '"
+const citacao = `Ju diz: "Nosso lema é 'estudar bastante!' " `
+
+console.log(cumprimento)
+console.log(citacao)
+
+console.log(`O estudante se chama ${estudante}`)
+
+//template string
+
+const senha = 'SenhaSegura123' + estudante.toUpperCase();
+console.log(senha);
