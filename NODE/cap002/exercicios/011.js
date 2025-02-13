59a01a78dfa96244163d0142946e7675786eceec
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-02-13 17:24:04 -0300

    ex

---
+++ NODE/cap002/exercicios/011.js
@@ -0,0 +1,7 @@
+const lista = [1,2,3]
+let i = 0
+
+for(let i of lista){
+    i++
+    console.log(i)
+}
---
+++ NODE/cap002/exercicios/012.js
@@ -0,0 +1,9 @@
+function imprimirIndicesElementos(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(`Índice: ${i}, Elemento: ${arr[i]}`);
+    }
+}
+
+// Exemplo de uso
+const minhaArray = [10, 20, 30, 40];
+imprimirIndicesElementos(minhaArray);
