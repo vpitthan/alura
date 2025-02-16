ef44597fdb61c16fad7421cec7170824897ab0ad
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2025-02-16 12:00:39 -0300

    sim

---
+++ ProjetosVini/duduégay/dudu.js
@@ -0,0 +1,9 @@
+let duduEGay = prompt('Dudu é gay?')
+while(duduEGay !== 'sim'){
+    console.log('Mentira');
+    duduEGay = prompt('Dudu é gay?')
+}
+if(duduEGay == 'sim'){
+    alert('Verdade');
+    
+}
